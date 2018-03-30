738cc53b8b2d5c9f8167853d6347f1b4abfa205c
victorvd <34987959+victorvd@users.noreply.github.com>
2018-03-30 18:45:06 -0500

    Add files via upload

---
+++ en/download.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Download - Path loss</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="../css/formas.css" rel="stylesheet" type="text/css">
+        <meta name="keywords" content="5G,frequency,band,spectrum">
+        <meta name="description" content="5G frequency band pathloss calculator">
+
+    </head>
+
+    <body>
+
+        <div class="container">
+            <header>
+                <div id="logo-pos"><img src="../images/posgrado.png"/></div>
+                <div id="logotipo"><img src="../images/logo.png"/></div>
+
+                <div id="language">
+                    <a class="" href="#" onclick="changeLanguage();">Español</a>
+                </div>
+
+                <div class="encabezado-interno">
+                    <h1>Master's degree in Telecommunications Engineering</h1>
+                    <div class="linea-sombreada-sup"></div>
+                    <div class="clearfix"></div>
+                    <div class="linea-sombreada-inf"></div>
+                </div>
+            </header>
+            <nav id="nav-bar" class="navbar navbar-toggleable-md navbar-light bg-faded">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="download.php">Download</a>
+                    </li>
+                </ul>
+            </nav>
+            <br>
+
+            <section>
+                <h3><a id="hdr_calc"></a>Scripts</h3>
+                <br>
+                <div>
+                    <ul style="color:#292B2C">
+                        <li><a href="/downloads/RMa.m" download>
+                                Rural Macrocell path loss</a></li>
+                        <li><a href="/downloads/UMa.m" download>
+                                Urban Macrocell path loss</a></li>
+                        <li><a href="/downloads/UMi.m" download>
+                                Urban Microcell path loss</a></li>
+                        <li><a href="/downloads/InH.m" download>
+                                Indoor Hotspot path loss</a></li>
+                        <li><a href="/downloads/Prlos.m" download>
+                                LoS Probability</a></li>
+                        <li><a href="/downloads/3GPP_PL.zip" download>
+                                3GPP path loss</a></li>
+                        <li><a href="/downloads/NYU_PL.zip" download>
+                                NYU path loss</a></li>
+                    </ul>
+                </div>
+                <br>
+                <h4><a id="hdr_3GPP"></a>Scripts can be executed with MATLAB®</h4>
+                
+                <br>
+                <div>
+                    <img src="../images/img_3NY.png" width="auto" height="300"/>
+                    <img src="../images/img_hist.png" width="auto" height="300"/>
+                </div>
+                <br>
+
+            </section>
+
+        </div>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+        <script src="js/results.js"></script>
+
+    </body>
+</html>
---
+++ js/results.js
@@ -0,0 +1,176 @@
+// JavaScript Document
+
+$("#txt_tab").change(function () { //función jQuery para escuchar un evento clic
+    multiples();
+});
+$(document).ready(function () {
+
+    //If the #txt_sce was set then apply multiples() function.
+    if ($("#txt_sce").find('option:selected').val() != ""){
+        multiples();
+    };
+
+    $("#frm_parm").submit(function () {
+        var scenario = escenario();
+        var pos_nam = "php/calc_" + scenario + ".php";
+
+        var datosForm = $(this).serialize();
+        $.post(pos_nam, datosForm, mostrResul);
+        return false;
+    });
+});
+
+function mostrResul(table) {
+
+    var callres = $('#call_result')[0]; // Usando [0] se obtiene un objeto DOM
+    var errDiv = $('#div_err')[0];
+
+    //Variables para los títulos del gráfico
+    var trc_nam = escenario() + ' pathloss';
+    var lay_tle = 'Pérdidas en el trayecto ' + escenario() + ' ';
+
+    //Manejar el error
+    if (table[0][0] !== "[") {
+        errDiv.style.display = "";
+        $("#lb_err").html("<font color=red>" + table + "</font>");
+        callres.style.display = "none";
+        return;
+    } //Fin de manejo de error
+
+    else {
+        var tabla = $("#table_freq")[0];
+        var PLoss = jQuery.parseJSON(table);
+        var ite = PLoss.length - 1;
+        errDiv.style.display = "none";
+        //Mostrar división de resultados: tabla y gráfico
+        callres.style.display = "";
+
+        //Cambiar la cabecera de la tabla de resultados para frec. o dist.        
+        switch ($("#txt_tab")[0].selectedIndex) {
+            case 0:
+                $("th#fr_ds").text("Frecuencia fc [MHz]");
+                //Cambiar título de eje X para la gráfica
+                var xaxe = 'Frecuencia de portadora [MHz]';
+                break;
+            case 1:
+                $("th#fr_ds").text("Frecuencia fc [MHz]");
+                var xaxe = 'Frecuencia de portadora [MHz]';
+                break;
+            case 2:
+                $("th#fr_ds").text("Distancia 3D [m]");
+                var xaxe = 'Distancia 3D [m]';
+                break;
+        }
+        ;
+    }
+    ;
+    $("#txt_plos").val(PLoss[0][1].toFixed(1));
+    $("#txt_shwf").val(PLoss[0][2].toFixed(1));
+    $("#txt_plsf").val((PLoss[0][1] + PLoss[0][2]).toFixed(1));
+
+    //Tabular
+    while (tabla.rows[2]) {
+        tabla.deleteRow(2);
+    }
+
+    for (var i = 0; i <= ite; i++) {
+        var fila = tabla.insertRow(tabla.rows.length);
+        fila.insertCell(0).innerHTML = i + 1;
+        fila.insertCell(1).innerHTML = (+(PLoss[i][0])).toFixed(2);
+        fila.insertCell(2).innerHTML = parseFloat(PLoss[i][1]).toFixed(2);
+    }
+
+    tabla.style.textAlign = "center";
+
+    //Graficar
+    var grafico = $("#grafic")[0];
+    //Seleccionar columnas 1 y 2 del array PLoss
+    var col3x = PLoss.map(function (value, index) {
+        return value[0];
+    });
+    var col3y = PLoss.map(function (value, index) {
+        return value[1];
+    });
+
+    var trace = {
+        x: col3x,
+        y: col3y,
+        name: trc_nam,
+        line: {
+            color: '#1b7e4c',
+            width: 1}
+    };
+
+    var layout = {
+        title: lay_tle
+                + $("#txt_los option:selected").text(), //Se obtiene "LOS" en lugar de "opt_los", p. ej.
+        titlefont: {
+            size: 24,
+            color: '#b8092a'
+        },
+        xaxis: {
+            type: 'log', //Graficar en plano semilogarítmico
+            title: xaxe,
+            titlefont: {
+                size: 18,
+                color: '#7f0000'
+            }
+        },
+        yaxis: {
+            title: 'Path Loss [dB]',
+            titlefont: {
+                size: 18,
+                color: '#7f0000'
+            }
+        }
+        //, showlegend: true
+    };
+
+    Plotly.newPlot(grafico, [trace], layout);
+}
+;
+
+function changeLanguage(language) {
+    var url = window.location.origin + language + location.pathname;
+    window.location.replace(url);
+}
+;
+//Función para habilitar los casilleros de frecuencia y distancia máximas
+function multiples() {
+
+    var fcDiv = $('#div_fcmx')[0]; // Usando [0] se obtiene un objeto DOM
+    var d2Div = $('#div_d2mx')[0];
+    var fcInp = $('#txt_fcmx');
+    var d2Inp = $('#txt_d2mx');
+    //$("#txt_fcmx").val(""); //$ usa funciones jQUERY para limpiar valores
+    //$("#txt_d2mx").val("");
+
+    //Mostrar los casilleros de frecuencia o distancia máximas
+    switch ($("#txt_tab")[0].selectedIndex) {
+        case 0:
+            fcDiv.style.display = "none";
+            d2Div.style.display = "none";
+            fcInp.attr('required', false);
+            d2Inp.attr('required', false);
+            break;
+        case 1:
+            fcDiv.style.display = "";
+            d2Div.style.display = "none";
+            fcInp.attr('required', true);
+            d2Inp.attr('required', false);
+            break;
+        case 2:
+            fcDiv.style.display = "none";
+            d2Div.style.display = "";
+            fcInp.attr('required', false);
+            d2Inp.attr('required', true);
+            break;
+    }
+    ;
+}
+function escenario() {
+
+    scenario = $("#txt_sce").find('option:selected').val();
+    scenario = scenario.substring(4);
+    return scenario;
+}
